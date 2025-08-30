1b7d4fae125e92633e58359a9cd9c81e68f64532
Nicolas O <nicolasolaya27@gmail.com>
2025-08-30 15:14:03 -0500

    First commit

---
+++ src/test/java/com/eam/mspackagesmanage/MspackagesmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.mspackagesmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MspackagesmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
